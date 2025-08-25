773c8845674b087f940f6bc4e3407a568513f96e
Patricio Pérez Pinto <patodeath@gmail.com>
2025-08-25 03:42:10 +0000

    feat: Initialize Bon Appetit backend project with Spring Boot
    - Add Maven Wrapper scripts (mvnw and mvnw.cmd) for easier project setup.
    - Create pom.xml with Spring Boot dependencies and configuration.
    - Implement main application class BonAppetitBackendApplication.
    - Configure application properties for PostgreSQL database connection.
    - Add use case diagram for project visualization in draw.io format.

---
+++ src/main/java/org/prezdev/bonappetit/BonAppetitBackendApplication.java
@@ -0,0 +1,13 @@
+package org.prezdev.bonappetit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonAppetitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BonAppetitBackendApplication.class, args);
+	}
+
+}
